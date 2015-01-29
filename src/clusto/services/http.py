be7e756c3f37cece544ae6d57c280070d013e6d1
Altan Alpay <altan@uber.com>
2015-01-29 20:12:22 +0000

    setattr returns '400 Element Not Found' if related object not found

--- src/clusto/services/http.py
+++ src/clusto/services/http.py
@@ -94,6 +94,7 @@ class EntityAPI(object):
 
         Requires HTTP parameters "key" and "value"
         Optional parameters are "subkey", "number" and "datatype"
+        where "datatype" could be "relation", "int" or string by default
         '''
         kwargs = dict(request.params.items())
         datatype = kwargs.get('datatype', None)
@@ -103,7 +104,7 @@ class EntityAPI(object):
             try:
                 kwargs['value'] = clusto.get_by_name(kwargs['value'].rsplit('/', 1)[1])
             except LookupError:
-                return Response(status=404, body='404 Not Found\n')
+                return Response(status=400, body='400 Element Not Found\n')
         elif datatype == 'int':
             kwargs['value'] = int(kwargs['value'])
         else:
